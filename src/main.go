edc790ccb5d64b6a05ea989949fef5f0e89376ef
Manuel Cuevas <imscuevas@gmail.com>
2022-07-27 01:09:33 -0500

    adding variable handling in go

--- src/main.go
+++ src/main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// Declaración de contantes
+	const pi float64 = 3.14
+	const pi2 = 3.1416
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Declaración de variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	area = base * altura
+
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+	var baseCuadrado int = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println("Area de un cuadrado:", areaCuadrado)
 }
